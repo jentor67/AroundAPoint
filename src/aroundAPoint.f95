a00ebca45e53a9aeb83efe365aaadfcdcecf95ce
John Major <john.jentor@gmail.com>
2025-02-05 23:30:58 -0600

    Starting OOPS

--- src/aroundAPoint.f95
+++ src/aroundAPoint.f95
@@ -3,12 +3,10 @@ Program aroundAPoint
    use gravityModule 
    implicit none
 
-   real :: pi =3.1359, Gc = 6.674083E-11
    real :: me = 5.972168E24, re = 6371000
    real :: F, g
 
    write(*,*) "Value of pi =",pi
-   write(*,*) "Value of G =", Gc
     
    g = Gc*me/(re**2)
       
--- src/gravityModule.f95
+++ src/gravityModule.f95
@@ -1,5 +1,17 @@
 !> \\file gravityModule.f95
 module gravityModule
   implicit none
+
+  real :: G = 6.674083E-11
+
+contains
+  
+  function acceleration(this) result(a)
+    real :: a
+
+    a = G*m/(r**2)
+  end function acceleration
+
+
 end module gravityModule
 
