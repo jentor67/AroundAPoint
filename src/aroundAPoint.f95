33dd8a0e70787a862808092a8b78ed551e6bc907
John Major <john.jentor@gmail.com>
2025-04-21 22:13:06 -0500

    Playing with 100 particles

--- src/aroundAPoint.f95
+++ src/aroundAPoint.f95
@@ -2,21 +2,67 @@
 Program aroundAPoint
    use gravityModule 
    implicit none
-
-   real :: force, gravity, distance1 
-   type(typeTest1) :: a 
+   integer :: n, m, k, particles, iterations
+   real :: force, gravity, fx, fy, fz, fxsum, fysum, fzsum, distpart
+   type(particle), dimension(1000000,100) :: partarray
    type(particle) :: sun
-   type(particle) :: earth
-   type(particle) :: jupiter
-  
-   call valuessun(sun)
-   call valuesearth(earth)
-   call valuesjupiter(jupiter)
-      
-   call forcevector(earth,sun,fx,fy,fz)
+   iterations = size(partarray,dim=1)-1
+   particles = size(partarray,dim=2)
 
-   write(*,*) fx, fy, fz
+   ! get initial positions of particles
+   do n = 1, particles
+     call getpartparm(partarray(1,n)) 
+   end do
+
+   ! pass mass to all elements
+   do n = 1, iterations
+     do m = 1, particles
+       partarray(n, m)%mass = partarray(1,m)%mass
+     end do
+   end do 
+
+   !  check distance
+   do n = 1, particles
+     do m = 1, particles
+       distpart = distance( partarray(1,n), partarray(1,m) )
+       if( distpart < .1 .and. n /= m) then
+         write(*,*) "Close", distpart, n, m
+       end if
+     end do
+   end do
+
+   call valuessun(sun) ! get value of sun
 
 
+   call printparticle( 1, partarray(1,1) )
+   call printparticle( 2, partarray(1,2) )
+   call printparticle( 3, partarray(1,3) )
+   call printparticle( 4, partarray(1,4) )
+
+   do n = 1, iterations
+     !write(*,*) "Iteration: ", n
+     do m = 1, particles
+       fxsum = 0
+       fysum = 0
+       fzsum = 0
+       do k = 1, particles
+         if( k /= m) then
+           call forcevector(partarray(n,m),partarray(n,k), fx, fy, fz)
+           fxsum = fxsum + fx
+           fysum = fysum + fy
+           fzsum = fzsum + fz
+         end if
+       end do
+       call velocitychange(partarray(n,m), partarray(n+1,m),fxsum,fysum,fzsum)
+       call positionchange(partarray(n,m), partarray(n+1,m) )
+     end do
+     !call printparticle(1, partarray(n+1,1))
+     !call printparticle(2, partarray(n+1,2))
+   end do
+   write(*,*) n, n*timedisp
+   call printparticle( 1, partarray(n-1,1) )
+   call printparticle( 2, partarray(n-1,2) )
+   call printparticle( 3, partarray(n-1,3) )
+   call printparticle( 4, partarray(n-1,4) )
 
 End Program aroundAPoint
--- src/gravityModule.f95
+++ src/gravityModule.f95
@@ -2,12 +2,15 @@
 module gravityModule
   implicit none
   public :: typeTest1, printTest1, acceleration, distance
-  public :: valuessun, valuesearth, valuesjupiter, forcevector
+  public :: valuessun, forcevector
+  public :: velocitychange,getpartparm, printparticle
   real :: mass 
   real :: gcu = 6.674083E-11
   real :: pie = 3.1415926535897932384626
   real :: mass_earth = 5.972168E24 
   real :: radius_earth = 6371000
+  real :: timedisp = .000001
+ 
 
   type particle
     real :: x
@@ -26,27 +29,48 @@ module gravityModule
 
 contains
 
-  subroutine valuesearth(sel)
+  subroutine getpartparm(sel)
     type(particle) sel
-    sel%x=0
-    sel%y=5
-    sel%z=0
-    sel%u=1
-    sel%v=0
-    sel%w=.5
-    sel%mass=.001
-  end subroutine valuesearth
+    real :: factor
+    factor = .00001
+    call random_number(sel%u)
+    call random_number(sel%v)
+    call random_number(sel%w)
+    call random_number(sel%x)
+    call random_number(sel%y)
+    call random_number(sel%z)
+    call random_number(sel%mass)
 
-  subroutine valuesjupiter(sel)
-    type(particle) sel
-    sel%x=10
-    sel%y=0
-    sel%z=4
-    sel%u=0
-    sel%v=.01
-    sel%w=.01
-    sel%mass=1
-  end subroutine valuesjupiter
+    sel%u = sel%u*factor
+    sel%v = sel%v*factor
+    sel%w = sel%w*factor
+    sel%x = sel%x*factor
+    sel%y = sel%y*factor
+    sel%z = sel%z*factor
+  end subroutine getpartparm
+
+
+  subroutine positionchange(sel, sel1)
+    type(particle) sel, sel1
+
+    sel1%x = sel%x+sel%u*timedisp
+    sel1%y = sel%y+sel%v*timedisp
+    sel1%z = sel%z+sel%w*timedisp
+
+  end subroutine positionchange
+ 
+  subroutine velocitychange(sel, sel1, fx, fy, fz)
+    real :: fx, fy, fz, masstime
+    type(particle) sel, sel1
+
+    masstime = timedisp/sel%mass
+
+    sel1%u = sel%u+fx*masstime
+    sel1%v = sel%v+fy*masstime
+    sel1%w = sel%w+fz*masstime
+    !write(*,*) sel%u, sel1%u, fx,sel%v, sel1%v, fy, sel%w, sel1%w, fz
+  end subroutine velocitychange
+
 
   subroutine valuessun(sel)
     type(particle) sel
@@ -56,16 +80,15 @@ contains
     sel%u=0
     sel%v=0
     sel%w=0
-    sel%mass=10
+    sel%mass=10000000.0
   end subroutine valuessun
 
   subroutine printTest1(at)
     type(typeTest1) at
 
-    write(*,*) 'Before ',at%xValue
     at%xValue = 3
-    write(*,*) 'After ',at%xValue
   end subroutine printTest1
+
 
   function acceleration(m,r) result(grav) 
     real :: grav, m, r
@@ -73,26 +96,36 @@ contains
     grav = gcu*m/(r**2)
   end function 
 
+
   function distance(a, b) result(r) 
     type(particle) a, b
     real :: r
 
     r = ( (b%x-a%x)**2 + (b%y-a%y)**2 + (b%z-a%z)**2 )**.5
-  end function 
+  end function
 
-  subroutine forceVector(a, b, fx, fy, fz) 
+
+  subroutine forcevector(a, b, fx, fy, fz) 
     real :: fx, fy, fz, dis1, force
     type(particle) a, b
 
     dis1 = distance(a,b)
 
-    force = Gcu*a%m*b%m/(dis1**2)
+    force = Gcu*a%mass*b%mass/(dis1**2)
 
     fx = force*(b%x-a%x)/dis1
     fy = force*(b%y-a%y)/dis1
     fz = force*(b%z-a%z)/dis1
     
   end subroutine forceVector
+
+
+  subroutine printparticle(i, sel)
+    integer :: i
+    type(particle) sel
+    write(*,*) "P ", i, " ", sel%x, sel%y, sel%z, sel%u, sel%v, sel%w, sel%mass
+  end subroutine printparticle
+
 
 end module gravityModule
 
